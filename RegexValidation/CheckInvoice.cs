9d5a78f973dc53ef21f75b7cbaa8346bdcb996df
GadkiyAbram <aabramovskii@gmail.com>
2020-01-03 11:03:02 +0500

    initial commit

---
+++ RegexValidation/CheckInvoice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexValidation
+{
+    public class CheckInvoice
+    {
+        private string inputString { get; set; }
+        private string pattern { get; set; }
+
+        public CheckInvoice(string inputPattern, string inputString)
+        {
+            this.inputString = inputString;
+            this.pattern = inputPattern;
+        }
+
+        public bool Validate()
+        {
+            bool output = true;
+
+            Match match = Regex.Match(pattern, inputString);
+
+            if (match.Success)
+            {
+                Console.WriteLine("Success");
+            }
+            else
+            {
+                Console.WriteLine("You're fucked");
+            }
+
+            return output;
+        }
+    }
+}
---
+++ RegexValidation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string InvoiceToCheck = "O/11/21";
+            string InvoicePattern = @"^([A-Z])\/([0-9]{2})\/([0-9]{2})$";
+            CheckInvoice Invoice = new CheckInvoice(InvoiceToCheck, InvoicePattern);
+
+            string CCDToCheck = "10707090-021017-0013452";
+            string CCDPattern = @"^([0-9]{8})-([0-9]{6})-([0-9]{7})$";
+            CheckInvoice CCD = new CheckInvoice(CCDToCheck, CCDPattern);
+
+            string DateToCheck = "2017-11-18";
+            string DatePattern = @"^([0-9]{4})-([0-9]{2}-([0-9]{2}))$";
+            //TOTO - Complete Date validation fully
+            CheckInvoice DateValidate = new CheckInvoice(DateToCheck, DatePattern);
+            DateValidate.Validate();
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+
+}
